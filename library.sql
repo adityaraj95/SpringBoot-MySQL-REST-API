474a600baa1e3bc45c56a72c4f09d6e166b43db7
adityaraj95 <chauhanadi.9@gmail.com>
2019-04-16 21:26:03 -0400

    changes made to the database structure

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 14, 2019 at 05:29 AM
+-- Generation Time: Apr 17, 2019 at 01:25 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -35,7 +35,8 @@ CREATE TABLE IF NOT EXISTS `books` (
   `B_author` varchar(255) NOT NULL,
   `B_pages` int(255) NOT NULL,
   `Status` int(255) NOT NULL,
-  `ISBN` int(255) NOT NULL,
+  `ISBN` mediumtext NOT NULL,
+  `Comments` varchar(255) NOT NULL,
   PRIMARY KEY (`Barcode`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -43,9 +44,9 @@ CREATE TABLE IF NOT EXISTS `books` (
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`B_Title`, `Barcode`, `B_author`, `B_pages`, `Status`, `ISBN`) VALUES
-('Logical Chess - Move By Move', 982377651, 'Irving Chernev', 256, 1, 713484640),
-('The Sicilian Accelerated Dragon - 20th Anniversary Edition', 12367123, ' Peter Heine Nielsen , Carsten Hansen ', 912, 1, 171785295);
+INSERT INTO `books` (`B_Title`, `Barcode`, `B_author`, `B_pages`, `Status`, `ISBN`, `Comments`) VALUES
+('The Sicilian Accelerated Dragon - 20th Anniversary Edition', 12367123, ' Peter Heine Nielsen , Carsten Hansen ', 912, 1, '171785295', 'Nice book on Chess Silician Method'),
+('How to Play Chess', 712399123, 'Claire Summerscale', 72, 1, '1465457674', 'Learn how to play basic');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
